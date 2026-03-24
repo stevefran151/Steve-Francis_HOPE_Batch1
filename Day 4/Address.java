1931339cfdcc02d0b6abed82530b2f180b500df8
stevefran151 <stevexjesa@gmail.com>
2026-03-24 08:50:33 +0530

    Day 4 tasks

---
+++ Day 4/Address.java
@@ -0,0 +1,9 @@
+public class Array{
+    public static void main(String[] args){
+        int[] arr4={1,2,3};
+        System.out.println(arr4);
+        System.out.print(arr[0].hashCode()==1);
+        Integer a=1;
+         
+       }
+}
